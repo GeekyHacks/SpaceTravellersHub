603ff63054ea4f0879952495f8b3876aa1d3e486
GeekyHacks <geekyhacks22@gmail.com>
2023-09-08 14:50:05 +0300

    Update routes for deployment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import planetImg from '../Assets/images/planet.png';
-import '../styles/css/NavBar.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import planetImg from "../Assets/images/planet.png";
+import "../styles/css/NavBar.css";
 
 const NavBar = () => {
-  const handleActive = (nav) => (nav.isActive ? '' : 'active-link');
+  const handleActive = (nav) => (nav.isActive ? "" : "active-link");
   const result = (
     <header className="navbar">
       <div className="logoInfo">
@@ -12,10 +12,31 @@ const NavBar = () => {
         <h1 className="navHeader">Space Traveller&apos;s Hub</h1>
       </div>
       <div className="links">
-        <NavLink className={(nav) => handleActive(nav)} to="/">Rockets</NavLink>
-        <NavLink className={(nav) => handleActive(nav)} to="/missions">Missions</NavLink>
-        <NavLink className={(nav) => handleActive(nav)} to="/dragons">Dragons</NavLink>
-        <NavLink id="border" className={(nav) => handleActive(nav)} to="/Myprofile">My Profile</NavLink>
+        <NavLink
+          className={(nav) => handleActive(nav)}
+          to="/SpaceTravellersHub/"
+        >
+          Rockets
+        </NavLink>
+        <NavLink
+          className={(nav) => handleActive(nav)}
+          to="/SpaceTravellersHub/missions"
+        >
+          Missions
+        </NavLink>
+        <NavLink
+          className={(nav) => handleActive(nav)}
+          to="/SpaceTravellersHub/dragons"
+        >
+          Dragons
+        </NavLink>
+        <NavLink
+          id="border"
+          className={(nav) => handleActive(nav)}
+          to="/SpaceTravellersHub/Myprofile"
+        >
+          My Profile
+        </NavLink>
       </div>
     </header>
   );
